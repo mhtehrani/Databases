bd3ad07ed8da44112f2aac380dcd3d21353cec96
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-27 20:42:12 -0700

    Create Create_Drop.sql
    Create and Drop tables in the database

---
+++ Create_Drop.sql
@@ -0,0 +1,8 @@
+CREATE TABLE INSTRUCTOR
+  (ins_id INTEGER PRIMARY KEY NOT NULL, 
+   lastname VARCHAR(15) NOT NULL, 
+   firstname VARCHAR(15) NOT NULL, 
+   city VARCHAR(15), 
+   country CHAR(2));
+   
+   Drop table INSTRUCTOR;
